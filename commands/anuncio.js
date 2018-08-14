40828f93c33e956a576f61eb16fd43a5c188ec72
rwedfdddfsdfsdwer <42014365+rwedfdddfsdfsdwer@users.noreply.github.com>
2018-08-14 15:59:57 -0300

    Update anuncio.js

--- commands/anuncio.js
+++ commands/anuncio.js
@@ -5,50 +5,49 @@ module.exports.run = async (client, message, args) =>{
    message.delete().catch(O_o=>{});
    if (!message.member.hasPermission('MANAGE_GUILD'))
       return message.reply("você não tem permissão!").then(msg => msg.delete(10000));
-   let kallyrole = message.guild.roles.find("name", "Kally");
+   let atlantic = message.guild.roles.find("name", "AtlanticMC - BOT");
    const comousar = new Discord.RichEmbed()
-      .setAuthor("Trix", client.user.avatarURL)
-      .setColor("#cc22a7")
-      .setFooter("© Trix - Paosz#5829")
-      .addField("Como usar:", "`t!anuncio <mensagem>`")
-      .addField("Permissão:", "A pessoa que for usar tem que possuir a permissão `Gerenciar servidor`")
+      .setAuthor("AtlanticMC - Bot", client.user.avatarURL)
+      .setColor("#4a2aed")
+      .setFooter("Atlantic")
+      .addField("Como usar:", "`!anuncio <mensagem>`")
       
    let mensg = args.join(" ");
    if(!mensg)
       return message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
-   if(!message.guild.roles.find("name", "Kally")) {
+   if(!message.guild.roles.find("name", "AtlanticMC - BOT")) {
         const anuncio = new Discord.RichEmbed()
-            .setColor('#cc22a7')
-            .setTitle(`Aviso ⚠️`)
+            .setColor('#4a2aed')
+            .setTitle(`⚠️ Atlantic - Anúncio `)
             
             .setDescription(mensg)
             
             .setTimestamp()
-            .setFooter(`Por: ${message.author.tag} - Trix`, message.author.avatarURL)
+            .setFooter(`Por: ${message.author.tag} - Anúncio`, message.author.avatarURL)
 
         message.channel.send("@everyone", anuncio)
         return;
     }
     if(kallyrole.color == "0"){
             const anuncio = new Discord.RichEmbed()
-                .setColor('#cc22a7')
-                .setTitle(`Aviso ⚠️`)
+                .setColor('#4a2aed')
+                .setTitle(`⚠️ Atlantic - Anúncio`)
                 
                 .setDescription(mensg)
                 
                 .setTimestamp()
-                .setFooter(`Por: ${message.author.tag} - Trix `, message.author.avatarURL)
+                .setFooter(`Por: ${message.author.tag} - Anúncio `, message.author.avatarURL)
 
             message.channel.send("@everyone", anuncio)
         }else{
             const anuncio = new Discord.RichEmbed()
-                .setColor(kallyrole.color)
-                .setTitle(`Aviso ⚠️`)
+                .setColor(atlantic.color)
+                .setTitle(`#4a2aed`)
                 
                 .setDescription(mensg)
                 
                 .setTimestamp()
-                .setFooter(`Por: ${message.author.tag} - Trix`, message.author.avatarURL)
+                .setFooter(`Por: ${message.author.tag} - Anúncio`, message.author.avatarURL)
 
             message.channel.send("@everyone", anuncio)
             return;
