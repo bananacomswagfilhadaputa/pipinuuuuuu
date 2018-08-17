c04037f91feb507198ba286ccb5d59a3a2c23241
rwedfdddfsdfsdwer <42014365+rwedfdddfsdfsdwer@users.noreply.github.com>
2018-08-16 21:50:24 -0300

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -1,6 +1,12 @@
 exports.run = function (bot, msg, args) {
     if (args.length < 1) {
-        throw 'Você deve escrever algo, burro';
+        throw 'Escreva algo bro';
     }
     msg.edit(args.join(' ').split('').reverse().join(''));
+};
+
+exports.info = {
+    name: 'reverse',
+    usage: 'reverse <text>',
+    description: 'Reverses the text you input'
 };
