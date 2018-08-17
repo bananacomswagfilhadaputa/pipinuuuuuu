0c3806970b4f54d2641216d0efafaa5b3a19c147
rwedfdddfsdfsdwer <42014365+rwedfdddfsdfsdwer@users.noreply.github.com>
2018-08-16 22:03:30 -0300

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -1,33 +1,72 @@
 const Discord = require("discord.js");
+module.exports.run = async (client, message, args) =>{
 
-module.exports.run = async (bot, message, args) => {
-  
-  let online = message.guild.members.filter(member => member.user.presence.status !== 'offline');
-  let day = message.guild.createdAt.getDate()
-  let month = 1 + message.guild.createdAt.getMonth()
-  let year = message.guild.createdAt.getFullYear()
-   let sicon = message.guild.iconURL;
-   let serverembed = new Discord.RichEmbed()
-   .setAuthor(message.guild.name, sicon)
-   .setFooter(`Servidor criado • ${day}.${month}.${year}`)
-   .setColor("#cc22a7")
-   .setTimestamp()
-   .setThumbnail(sicon)
-   .addField("ID:", message.guild.id, true)
-   .addField("Nome:", message.guild.name, true)
-   .addField("Dono:", message.guild.owner.user.tag, true)
-   .addField("Região:", message.guild.region, true)
-   .addField("Channels", message.guild.channels.size, true)
-   .addField("Membros:", message.guild.memberCount, true)
-   .addField("Bots:", message.guild.members.filter(m => m.user.bot).size, true)
-   .addField(":white_circle: Onlines:", online.size, true)
-   .addField("Cargos:", message.guild.roles.size, true);
-   message.channel.send(serverembed);
+    if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply(`você não tem permissão! ${emojinop}`).then(msg => msg.delete(5000));
+    message.delete().catch(O_o=>{});
+    const mutarcanalmsg = new Discord.RichEmbed()
+        .setTitle("Gerenciamento do chat.")
+        .setDescription(`Deseja mutar este canal? reaja ao emoji :mute:\nDeseja desmutar este canal? reaja ao emoji :loud_sound:\nEstá com duvidas reaja ao emoji :question:`)
+        .setColor("#cc22a7")
+        .setFooter(`Atlantic - Ira mutar o canal: ${message.channel.name}`)
+    let mensg = args.join(" ");
+    if(!mensg){
+        message.channel.send(mutarcanalmsg).then(msg=> {
+            msg.react("🔇").then(r => {
+                msg.react("🔊")
+                msg.react("❓")
+                msg.delete(78000).catch(O_o=>{});
 
-}
-exports.help = {
-  name: "serverinfo",
-  aliases: [
-      'sv'
-  ]
-  }
+                const podemutar = (reaction, user) => reaction.emoji.name === '🔇' && user.id === message.author.id;
+                const podedesmutar = (reaction, user) => reaction.emoji.name === '🔊' && user.id === message.author.id;
+                const info = (reaction, user) => reaction.emoji.name === '❓' && user.id === message.author.id;
+    
+                const podemutarL = msg.createReactionCollector(podemutar, { time: 60000 });
+                const podedesmutarL = msg.createReactionCollector(podedesmutar, { time: 60000 });
+                const infoL = msg.createReactionCollector(info, { time: 60000 });
+                
+    
+                podemutarL.on('collect', r=> {
+                    msg.delete();
+                    let role = message.guild.roles.find("name", "@everyone");
+                    message.channel.overwritePermissions(role, {SEND_MESSAGES: false});
+                    message.channel.send(`Este canal foi mutado por: __${message.author}__`)
+                    return;
+                })
+                podedesmutarL.on('collect', r=> {
+                    msg.delete();
+                    let role = message.guild.roles.find("name", "@everyone");
+                    message.channel.overwritePermissions(role, {SEND_MESSAGES: true});
+                    message.channel.send(`Este canal foi desmutado por: __${message.author}__`)
+                    return;
+                })
+                infoL.on('collect', r=> {
+                    msg.delete();
+                    const comousar = new Discord.RichEmbed()
+                        .setAuthor("Kally", client.user.avatarURL)
+                        .setDescription(`Ao executar, ira mutar ou desmutar o devido canal.`)
+                        .setColor("#cc22a7")
+                        .setFooter("Atlantic  - Paosz#5829")
+                        .addField("Como usar:", "`t!chat <on/off>`")
+                    message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
+                })
+            })
+        }).catch(O_o=>{});
+    }
+    if(mensg){
+        if(args[0] == "OFF" || args[0] == "off"){
+            let role = message.guild.roles.find("name", "@everyone");
+            message.channel.overwritePermissions(role, {SEND_MESSAGES: false});
+            message.channel.send(`Este canal foi mutado por: __${message.author}__`)
+            return;
+        }else if(args[0] == "ON" || args[0] == "on"){
+            let role = message.guild.roles.find("name", "@everyone");
+            message.channel.overwritePermissions(role, {SEND_MESSAGES: true});
+            message.channel.send(`Este canal foi desmutado por: __${message.author}__`)
+            return;
+        } else{
+        message.reply("por favor use assim: `t!chat off` ou `t!chat on`!").then(msg => msg.delete(8000));
+        }
+    }
+    
+    
+ }
