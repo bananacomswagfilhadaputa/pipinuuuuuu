353759d95a30fd231c14f432eaeabc15cb35a06c
rwedfdddfsdfsdwer <42014365+rwedfdddfsdfsdwer@users.noreply.github.com>
2018-08-21 15:57:06 -0300

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
       const embed = new Discord.RichEmbed() 
     .setAuthor('Votação', "https://cdn.discordapp.com/attachments/467178630666256398/471106934259318784/success.gif")
     .setDescription("" + args.join(" "))
-    .setColor('RANDOM')
+    .setColor('#4a2aed')
     .setTimestamp()
     .setFooter(`Por: ${message.author.username}`, message.author.avatarURL); 
         message.delete().catch();
