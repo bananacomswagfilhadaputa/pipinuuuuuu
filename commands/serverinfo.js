8d792c3d983b1e1dc3a9c17b8ea714290bd0eec8
rwedfdddfsdfsdwer <42014365+rwedfdddfsdfsdwer@users.noreply.github.com>
2018-08-22 15:46:25 -0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,33 +1,65 @@
-const Discord = require("discord.js");
+var Discord = require('discord.js')
 
-module.exports.run = async (bot, message, args) => {
-  
-  let online = message.guild.members.filter(member => member.user.presence.status !== 'offline');
-  let day = message.guild.createdAt.getDate()
-  let month = 1 + message.guild.createdAt.getMonth()
-  let year = message.guild.createdAt.getFullYear()
-   let sicon = message.guild.iconURL;
-   let serverembed = new Discord.RichEmbed()
-   .setAuthor(message.guild.name, sicon)
-   .setFooter(`Servidor criado • ${day}.${month}.${year}`)
-   .setColor("#cc22a7")
-   .setTimestamp()
-   .setThumbnail(sicon)
-   .addField("ID:", message.guild.id, true)
-   .addField("Nome:", message.guild.name, true)
-   .addField("Dono:", message.guild.owner.user.tag, true)
-   .addField("Região:", message.guild.region, true)
-   .addField("Channels", message.guild.channels.size, true)
-   .addField("Membros:", message.guild.memberCount, true)
-   .addField("Bots:", message.guild.members.filter(m => m.user.bot).size, true)
-   .addField(":white_circle: Onlines:", online.size, true)
-   .addField("Cargos:", message.guild.roles.size, true);
-   message.channel.send(serverembed);
+var cor = "#7401DF"
 
+exports.run = (client,message,args,)=>{
+    const moment = require('moment')
+    moment.locale('pt-br')
+    const msg = message;
+    var time = Date.now();
+    if (!message.guild.available) return;
+    moment.locale('pt-br');
+
+
+    let guild;
+
+    if (args[0]) {
+        guild = client.guilds.get(args[0]);
+    } else {
+        guild = message.guild;
+    }
+
+    let serverRegion = {
+        'amsterdam': ':flag_nl: Amsterdã',
+        'brazil': ':flag_br: Brasil',
+        'eu-central': ':flag_eu: Europa Central',
+        'eu-west': ':flag_eu: Europa Ocidental',
+        'frankfurt': ':flag_de: Frankfurt',
+        'hongkong': ':flag_hk: Hong Kong',
+        'japan': ':flag_jp: Japão',
+        'london': ':flag_gb: Londres',
+        'russia': ':flag_ru: Russia',
+        'singapore': ':flag_sg: Singapura',
+        'sydney': ':flag_au: Sydney',
+        'us-central': ':flag_us: EUA Central',
+        'us-east': ':flag_us: EUA Oriental',
+        'us-west': ':flag_us: EUA Ocidental',
+        'us-south': ':flag_us: EUA Sul',
+    }[guild.region];
+
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    
+     .setFooter(message.author.tag,message.author.avatarURL )
+        .setTimestamp(new Date())
+    .setColor("4a2aed")
+    .setThumbnail(sicon)
+    .setTitle(`${message.guild.name}`)
+    .addField(`:hammer_pick: Informações sobre o discord`, "Veja as informações do servidor!")
+
+    .addField(":upside_down: __Guild__ __ID__:", message.guild.id , true )
+    .addField(":gem: __Dono__ :", message.guild.owner , true )
+    .addField(":slight_smile: __Dono__ __ID__:", message.guild.owner.id  , true )
+    .addField(":pencil: __Criado__:", message.guild.createdAt , true )
+    .addField(":bell: __Região__:", `${serverRegion}` , true)
+    .addField(":star: __Emojis__: ", message.guild.emojis.size , true)
+    .addField(":radio: __Canais__", message.guild.channels.size, true)
+    .addField(":fire: __Membros__:", message.guild.memberCount , true)
+.addField(":white_circle:  __Onlines__:", `${guild.members.filter(o => o.presence.status === 'online').size}`, true)
+.addField(":o: __Ocupados__:",`${guild.members.filter(dnd => dnd.presence.status === 'dnd').size}`, true)
+.addField(":open_mouth: __Ausentes__:", `${guild.members.filter(i => i.presence.status === 'idle').size}`, true) 
+.addField(":black_circle:  __Offlines__:", `${guild.members.filter(off => off.presence.status === 'offline').size} `, true)
+    
+      
+    return message.channel.send(serverembed);
 }
-exports.help = {
-  name: "serverinfo",
-  aliases: [
-      'sv'
-  ]
-  }
