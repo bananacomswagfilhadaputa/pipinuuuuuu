a5dd75231ec6ff8d2f463773daa02d7ca6183887
rwedfdddfsdfsdwer <42014365+rwedfdddfsdfsdwer@users.noreply.github.com>
2018-08-23 15:15:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,48 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const fs = require('fs');
+
+var hoje = new Date();
+            var dd = hoje.getDate();
+            var mm = hoje.getMonth()+1;
+            var hh = hoje.getHours()-3;
+            var min = hoje.getMinutes();
+            var ss = hoje.getSeconds();
+            var yyyy = hoje.getFullYear();
+            if(dd<10){
+                dd = '0'+dd;
+            }
+            if (mm<10) {
+                mm = '0'+mm;
+            }
+            if (hh<10){
+		if(hh<01){
+		    hh = 3+hh;
+		}
+                hh = '0'+hh;
+            }
+            if (min<10){
+                min = '0'+min;
+            }
+            var hoje = dd+ '/' +mm+ '/' +yyyy + ' às ' + hh + ':' + min;
+	    var hojee = dd+ '/' +mm+ '/' +yyyy;
+
+var prefix = "!";
+
+client.on("ready", () => {
+	
+    client.channels.get('478995804711813150').send(':grinning:  | O bot foi reiniciado com sucesso!\n\nData: ' + hoje).then(msg => {
+    	msg.delete(50000)
+    })
+	    
+    const activities = ['no mc-atlantic.tk', 'no Atlantic', 'criado por Paosz#5829']
+    let counter = 0
+    setInterval(function() {
+        client.user.setGame(activities[counter], "https://twitch.tv/paoszzz")
+        counter+= 1
+        counter %= activities.length
+    }, 10000)
+});
  
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
